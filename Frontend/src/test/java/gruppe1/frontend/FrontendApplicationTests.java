a7bade1326cae3d7c6f63ff033142b0a9a54bf50
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-11-18 12:56:45 +0100

    Frontend

---
+++ Frontend/src/test/java/gruppe1/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe1.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
