4a1ae371d6e2fe0266fe169c8c256fa986e5b864
Sverri Reinert <sverrireinert@gmail.com>
2021-11-21 19:57:15 +0100

    stuff

--- Backend/src/main/java/gruppe1/backend/BackendApplication.java
+++ Backend/src/main/java/gruppe1/backend/BackendApplication.java
@@ -1,6 +1,9 @@
 package gruppe1.backend;
 
 
+import com.merakianalytics.orianna.Orianna;
+import com.merakianalytics.orianna.types.core.staticdata.Champions;
+import gruppe1.backend.models.Champion;
 import gruppe1.backend.models.Summoner;
 import gruppe1.backend.repositories.RiotRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +16,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(BackendApplication.class, args);
+
+        System.out.println(Orianna.getChampions());
+
+
+
     }
 
 
