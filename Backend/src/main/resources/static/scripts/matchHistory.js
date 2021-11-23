4308037ef51f5a8aa987c1e30a8aaedb6b65626f
Kringelum2 <p.kringelum@gmail.com>
2021-11-23 11:43:02 +0100

    Matches

--- Backend/src/main/resources/static/scripts/matchHistory.js
+++ Backend/src/main/resources/static/scripts/matchHistory.js
@@ -1,11 +1,64 @@
+const matchesTableBody = document.getElementById("matchHistory-tbody");
 const queryString = window.location.search;
 const URLParams = new URLSearchParams(queryString);
 const puuid = URLParams.get("puuid");
 
-fetch("https://localhost:8080/matches/" + puuid)
+fetch("http://localhost:8080/matches/" + puuid)
     .then(response => response.json())
-    .then(summoner => {
+    .then(matches => {
+        matches.map(createMatchTableRow);
         document.getElementById("summoner-name").innerText = summoner.name;
         document.getElementById("summoner-note").innerText = summoner.note;
         document.getElementById("summoner-level").innerText = summoner.level;
     })
+
+function createMatchTableRow(matches) {
+    const matchTableRow = document.createElement("tr");
+    matchTableRow.id = matches.id;
+
+    matchesTableBody.appendChild(matchTableRow);
+
+    constructMatchesTableRow(matchTableRow, matches);
+
+}
+
+
+function constructMatchesTableRow(matchesTableRow, match) {
+    matchesTableRow.innerHTML = `
+<td>
+<p class="row-matches-win">${escapeHTML(match.win.toString())}</p>
+</td>
+<td>
+<p class="row-matches-matchStart">${escapeHTML(match.matchStart.toString())}</p>
+</td>
+<td>
+<p class="row-matches-duration">${escapeHTML(match.duration.toString())}</p>
+</td>
+<td>
+<p class="row-matches-kills">${escapeHTML(match.kills.toString())}</p>
+</td>
+<td>
+<p class="row-matches-deaths">${escapeHTML(match.deaths.toString())}</p>
+</td>
+<td>
+<p class="row-matches-assists">${escapeHTML(match.assists.toString())}</p>
+</td>
+<td>
+<p class="row-matches-gameMode">${escapeHTML(match.gameMode)}</p>
+</td>
+<td>
+<p class="row-matches-champion">${escapeHTML(match.championName)}</p>
+</td>
+<td>
+<p class="row-matches-note">${escapeHTML(match.note)}</p>
+</td>
+<td>
+<button id="updateNote-button-${match.matchId}">📝</button>
+</td>
+<td>
+<button onclick="deleteMatch(${match.id})">❌</button>
+</td>
+`;
+
+
+}
