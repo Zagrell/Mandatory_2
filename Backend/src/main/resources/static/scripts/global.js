059b0ead93f4da8ca77625ed91a134db6afe062e
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-11-23 14:45:42 +0100

    A little smthng

--- Backend/src/main/resources/static/scripts/global.js
+++ Backend/src/main/resources/static/scripts/global.js
@@ -22,7 +22,7 @@ navBar.innerHTML=`
     </a>
 `;
 
-// SNOWFLAKES
+// SNOWFLAKES med hjælp fra https://www.youtube.com/watch?v=_ARGxz_cU_o&ab_channel=FlorinPop (fordi det snart er jul)
 
 createSnowFlakes();
 
@@ -35,7 +35,7 @@ function createSnowFlakes(){
     snow_flake.style.left = Math.random() * window.innerWidth + `px`;
     snow_flake.style.animationDuration = Math.random() * 3 + 2 + `s`;
     // between 2 - 5 seconds
-    snow_flake.style.opacity = Math.random()
+    snow_flake.style.opacity = Math.random();
     snow_flake.style.fontSize = Math.random() * 10 + 10 + `px`;
 
 
